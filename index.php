58f0b71d6163a531a7a4c5b4aab2a0098e6baa3e
Bruno Souza <66313781+BrunoVini@users.noreply.github.com>
2023-06-16 19:34:15 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Define o fuso horário padrão para o Brasil
+date_default_timezone_set('America/Sao_Paulo');
+
+// Exibe uma mensagem de boas-vindas
+echo "<h1>Olá, bem-vindo ao meu site!</h1>";
+
+// Exibe a data atual no formato dd/mm/yyyy
+echo "<p>Hoje é " . date('d/m/Y') . "</p>";
+?>
