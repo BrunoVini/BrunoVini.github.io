4d84652ffa9da256bc95bade5d41fbe8903ace99
BrunoVini <brunoluz858@gmail.com>
2023-12-07 13:00:46 -0300

    add archives

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <?php
-// Define o fuso horário padrão para o Brasil
-date_default_timezone_set('America/Sao_Paulo');
-
-// Exibe uma mensagem de boas-vindas
-echo "<h1>Olá, bem-vindo ao meu site!</h1>";
-
-// Exibe a data atual no formato dd/mm/yyyy
-echo "<p>Hoje é " . date('d/m/Y') . "</p>";
+    include('config.php');
+    $url = isset($_GET['url']) ? $_GET['url'] : 'home';
+    $urlArr = explode('/',$url);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bruno Souza - Portifólio</title>
+</head>
+
+<body>
+
+
+  <?php
+
+        if(file_exists("pages/". @$urlArr[0]. ".php")) {
+            include("pages/". $urlArr[0]. ".php");
+        } else if(@$urlArr[0] == 'news' || @$urlArr[1] == 'unique-news' || @$urlArr[2] == 'noticia') {
+            include("pages/news/unique-news.php");
+        } else {
+            include("pages/404.php");        
+        }
+
+    ?>
+
+
+</body>
+
+</html>
